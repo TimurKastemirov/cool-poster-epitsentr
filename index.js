16bae6ba93793925d7631cf6d302a39e388246df
Tymur Kastemirov <tamerlan8.05.92@gmail.com>
2021-02-07 23:22:19 +0300

    prepare csv file

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+const csv = require('csv-parser');
+const fs = require('fs');
+
+const results = [];
+let counter = 0;
+
+fs.createReadStream('csv-prepared.csv')
+    .pipe(csv())
+    .on('data', (data) => {
+        // console.log(data);
+        if (counter < 5) {
+            results.push(data);
+        }
+        counter++;
+    })
+    .on('end', () => {
+        fs.writeFileSync('./qqq.json', JSON.stringify(results));
+    });
